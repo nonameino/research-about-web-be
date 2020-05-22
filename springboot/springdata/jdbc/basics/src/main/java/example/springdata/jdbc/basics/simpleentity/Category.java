1a2537ded74011657fb214030ff8867ef2198104
nonameino_work <hungnh1812@gamil.com>
2020-05-22 18:01:51 +0700

    - update project structure

--- springboot/springdata/jdbc/basics/src/main/java/example/springdata/jdbc/basics/simpleentity/Category.java
+++ springboot/springdata/jdbc/basics/src/main/java/example/springdata/jdbc/basics/simpleentity/Category.java
@@ -3,5 +3,10 @@ package example.springdata.jdbc.basics.simpleentity;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE, onConstructor = @__(@PersistenceConstructor))
 public class Category {
+
+    @Id
+    @Wither
+    private final Long id;
 }
