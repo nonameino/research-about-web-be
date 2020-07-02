8de70dd5a666aec259f2adf26e3f567fb4b734a9
Hung Nguyen Huy <hung.nguyenhuy@gameloft.com>
2020-07-02 16:33:15 +0700

    [SpringbootBasic][Lombok] + Add some annotation

--- spring-boot-basic/lombok/src/main/java/example/springboot/basic/lombok/Entity.java
+++ spring-boot-basic/lombok/src/main/java/example/springboot/basic/lombok/Entity.java
@@ -1,11 +1,19 @@
 package example.springboot.basic.lombok;
 
-import lombok.Getter;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 @Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 @Slf4j
 public class Entity {
+    @NonNull
     private Integer id;
+
     private String name;
 }
