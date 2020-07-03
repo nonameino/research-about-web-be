ef76cfe01f15f878f16365037254d55e933315e0
Hung Nguyen Huy <hung.nguyenhuy@gameloft.com>
2020-07-03 10:44:45 +0700

    [SpringBootBasicc][PropertiesFiles] - Change application.yml to application.properties, don't know why application can't load yml file.

--- spring-boot-basic/properties-files/src/main/java/example/springboot/basic/propertiesfiles/Entity.java
+++ spring-boot-basic/properties-files/src/main/java/example/springboot/basic/propertiesfiles/Entity.java
@@ -1,8 +1,10 @@
 package example.springboot.basic.propertiesfiles;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @ConfigurationProperties(prefix = "simple")
 public class Entity {
@@ -10,20 +12,24 @@ public class Entity {
         private String b;
 
         public String getB() {
+            log.info("getB");
             return b;
         }
 
         public void setB(String b) {
-            b = b;
+            log.info("setB");
+            this.b = b;
         }
     }
     private A a;
 
     public A getA() {
+        log.info("getA");
         return a;
     }
 
     public void setA(A a) {
-        a = a;
+        log.info("setA");
+        this.a = a;
     }
 }
